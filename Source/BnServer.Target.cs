e379d6baeeeb938db7f88f818636b441b683b146
Adipa Wijayathilaka <adipamihi@gmail.com>
2019-12-22 12:24:35 -0500

    Added dedicated server as build target

---
+++ Source/BnServer.Target.cs
@@ -0,0 +1,15 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+[SupportedPlatforms(UnrealPlatformClass.Server)]
+public class BnServerTarget : TargetRules
+{
+	public BnServerTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Server;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("Bn");
+	}
+}
