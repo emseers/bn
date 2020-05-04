d572cae928e86ed6c3fb96a876947f45ea1411a4
Adipa Wijayathilaka <adipamihi@gmail.com>
2020-05-04 11:01:39 -0400

    Added interface for slide interacts (has rate param)

---
+++ Source/Bn/BnSlideInteractive.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BnSlideInteractive.h"
+
+// Add default functionality here for any IBnSlideInteractive functions that are not pure virtual.
